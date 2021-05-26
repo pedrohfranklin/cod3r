0bd2f8a230af8cd6889cfbf0f07a1206e68d0699
pedrohfranklin <77399591+pedrohfranklin@users.noreply.github.com>
2021-05-26 16:08:31 -0300

    Delete Coder directory

--- Coder/fundamentos/ResoluçãoLista/5.js
+++
@@ -1,6 +0,0 @@
-function formatarValor(decimal){
-    valorEmReais = `R$ ${decimal.toFixed(2).toString().replace(".", ",")}`
-    console.log(valorEmReais)
-}
-
-formatarValor(0.2+0.5)
--- Coder/fundamentos/ResoluçãoLista/8.js
+++
@@ -1,5 +0,0 @@
-let pontuacao = "30, 40, 20, 4, 51, 25, 42, 38, 56, 0";
-
-function avalia(pontuacao){
-    
-}
