00b4b3b5f8b8b70c76797ee697c3c04a7b2ee71f
Pedro Franklin <pfranklinms@poli.ufrj.br>
2021-05-26 15:46:31 -0300

    Exercício 26

--- fundamentos/ResoluçãoLista/23.js
+++ fundamentos/ResoluçãoLista/23.js
@@ -1,13 +1,3 @@
 function notaFinal (codAluno, nota1, nota2, nota3){
-    let notas = []
-    notas.push(nota1)
-    notas.push(nota2)
-    notas.push(nota3)
-    notas.sort((a,b) => a < b ? 1 : -1)
-
-    let media = (notas[0]*4+notas[1]*3+notas[2]*3)/10
-
-    console.log(`Aluno: ${codAluno}. Notas: ${nota1}, ${nota2}, ${nota3}. Média final é: ${media}. O aluno está ${media < 5 ? 'Reprovado' : 'Aprovado'}`)
+    m
 }
-
-notaFinal (250, 5, 9, 7)
--- fundamentos/ResoluçãoLista/24.js
+++ fundamentos/ResoluçãoLista/24.js
@@ -1,9 +1,9 @@
-function olaMundo(){
-	let i = 0
-		while (i<11){
-			console.log('Olá Mundo!!');
-			i++
-		}
+function olaMundo() {
+  let i = 0;
+  while (i < 11) {
+    console.log("Olá Mundo!!");
+    i++;
+  }
 }
 
-olaMundo()
+olaMundo();
